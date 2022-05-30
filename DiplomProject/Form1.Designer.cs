8e6bba549b0858e1a20811c591bfed266d23c3be
Destination <jekanotevgeny@gmail.com>
2022-05-30 17:54:01 +0300

    add excel

--- DiplomProject/Form1.Designer.cs
+++ DiplomProject/Form1.Designer.cs
@@ -38,13 +38,14 @@ namespace DiplomProject
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(10, 310);
+            this.button1.Location = new System.Drawing.Point(10, 187);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(102, 56);
             this.button1.TabIndex = 0;
@@ -128,11 +129,22 @@ namespace DiplomProject
             this.label3.TabIndex = 8;
             this.label3.Text = "Final point";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 54);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Export to excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.dataGridView2);
@@ -162,6 +174,7 @@ namespace DiplomProject
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
